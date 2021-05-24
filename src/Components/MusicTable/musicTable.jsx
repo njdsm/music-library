f647ab2ae5b0684f55e24020cfd1c4eae3e78f5c
Nathanial Johnson <nathanial.r.johnson@gmail.com>
2021-05-24 09:25:31 -0500

    updates to table

--- src/Components/MusicTable/musicTable.jsx
+++ src/Components/MusicTable/musicTable.jsx
@@ -1,35 +1,36 @@
-
 const MusicTable = (props) => {
     return(
         <div>
-            <table>
-                <tr> 
-                    <th>Song Id</th>
-                    <th>Song Name</th>
-                    <th>Artist</th>
-                    <th>Album</th>
-                    <th>Release Date</th>
-                    <th>Genre</th>
-                    <th>Likes</th>
-                    <th></th>
-                </tr>
-                {props.songs.map((song) =>
-                    <tr>
-                        <td>{song.id}</td>
-                        <td>{song.title}</td>
-                        <td>{song.artist}</td>
-                        <td>{song.album}</td>
-                        <td>{song.release_date}</td>
-                        <td>{song.genre}</td>
-                        <td>{song.likes}</td>
-                        <td><button onClick={props.deleteSong.bind(this, song.id)}>Delete</button></td>
-                    </tr> 
-                )}
+            <table id="sortedTable" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">
+                <thead>
+                    <tr> 
+                        <th>Song Id</th>
+                        <th>Song Name</th>
+                        <th>Artist</th>
+                        <th>Album</th>
+                        <th>Release Date</th>
+                        <th>Genre</th>
+                        <th>Likes</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.songs.map((song) =>
+                        <tr>
+                            <td>{song.id}</td>
+                            <td>{song.title}</td>
+                            <td>{song.artist}</td>
+                            <td>{song.album}</td>
+                            <td>{song.release_date}</td>
+                            <td>{song.genre}</td>
+                            <td>{song.likes}</td>
+                            <td><button onClick={props.deleteSong.bind(this, song.id)}>Delete</button></td>
+                        </tr> 
+                    )}
+                </tbody>   
             </table>
         </div>
     )
 }
-
-
 
 export default MusicTable
